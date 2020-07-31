5d8e9fde803525e53508428b948e01a01a7cd6f7
Pissboy-Jeje <ambiyasuci@gmail.com>
2020-07-31 18:06:53 +0700

    eaeaea

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Header from "./components/Header";
+import axios from "axios";
+import Routes from "./routes";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    books: [],
+    isLoading: false,
+  };
+  componentDidMount() {
+    const context = this;
+    this.setState({isLoading: true}, () =>
+      axios.get("https://calm-dusk-11524.herokuapp.com/library").then(res => {
+        console.log(res);
+        context.setState({
+          books: res.data,
+          isLoading: false,
+        })
+      }
+      )
+    )
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div
+          className="container"
+          style={{ paddingTop: "84px", paddingBottom: "24px" }}
+        >
+          <Routes />
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ListBooks.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Books from "./Books";
+import axios from "axios";
+
+class ListBooks extends Component {
+  // TODO: Buat sebuah state berisi books dengan nilai default array kosong []
+  //       dan isLoading dengan nilai default false
+  state = {
+    // isi disini
+    books:[],
+    isLoading:false
+    
+  };
+
+  componentDidMount() {
+    const context = this
+    // TODO: Panggil API dengan method GET untuk mendapat semua data yang terdapat pada database.
+    // isi disini
+    this.setState({isLoading: true}, () =>
+    axios
+    .get("https://calm-dusk-11524.herokuapp.com/library")
+    .then(res =>{
+      console.log(res);
+      context.setState({
+        books: res.data,
+        isLoading: false
+      })
+    }
+    )
+    )    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          {this.state.isLoading? <div>Lagi loading...</div>:
+          // TODO: Panggil component BOOKS dengan membawa state books sebagai props books
+          // isi disini
+          <Books
+          books={this.state.books}
+          />
+          }
+        </div>{" "}
+      </div>
+    );
+  }
+}
+
+export default ListBooks;
